3d9b15e71d5f68e1ff526091670f675d867d7b48
Andrew Thomas <apt41790@gmail.com>
2018-02-11 15:53:18 -0500

    Everything written, nothing tested. Should be more or less okay, but requires lots and lots of testing.

--- AndrewThomas_CIT284_Project1(CreditCard)/Main.cpp
+++ AndrewThomas_CIT284_Project1(CreditCard)/Main.cpp
@@ -20,10 +20,13 @@ Change Log:
 				 - Started overloaded extraction operator for reading from file [unfinished]
 				 - Fixed off-by-one error in generating card number (card numbers were 17 digits)
 			2/8  - Added error handling for most common error cases
-				 - Adjusted overloaded >> operator. Compiles now. [UNTESTED]
-			2/10 - Added write to file upon creation of an account [UNTESTED]
+				 - Adjusted overloaded >> operator. Compiles now. [Seems to work]
+			2/10 - Added write to file upon creation of an account [Seems to work]
 			2/11 - Trying to get this thing working as intended
 				 - Moved String checking on symbol to main() (removed from CC constructor)
+				 - Removed overloaded istream operator. Not necessary.
+				 - Fixed the constructor and file searching. [UNTESTED]
+				 - NEEDS LOTS AND LOTS OF TESTING
 */
 
 #include <iostream>
@@ -36,6 +39,7 @@ Change Log:
 
 using namespace std;
 
+
 class CreditCard {
 private: string accountNumber;
 private: double maxCredit;
@@ -43,12 +47,20 @@ private: double availableCredit;
 
 
 public: CreditCard(int firstDigit) {
-	string cardNumber = generateCardNumber(firstDigit);
+	// Takes in an int, determined in main() by which symbol is provided by user
 
+	// Generates a card number with firstDigit as the first digit
+	string cardNumber = generateCardNumber(firstDigit);
+	
+	// Stores the cardNumber as the account number for the object
 	this->accountNumber = cardNumber;
+	// The check digit is the last digit of the card number
 	char checkDigitChar = cardNumber.at(15);
+
+	// Subtract the value of '0' from the check digit to convert to int
 	int checkDigitInt = checkDigitChar - '0';
 
+	// Check digit determines credit line, 4 or lower gets 1,000, above 4 gets 500
 	if (checkDigitInt <= 4) {
 		maxCredit = 1000;
 	}
@@ -56,26 +68,26 @@ public: CreditCard(int firstDigit) {
 		maxCredit = 500;
 	}
 
+	// Set currently available credit to max credit
+	// These valuse must be separate to disallow accounts to have available credit greater than max credit
 	availableCredit = maxCredit;
 
 
 	
 
 }
-public: CreditCard(string acctNum, double amount, string fileName) {
-	/*
-	TODO:
-	***Search file for account number***
-	If found, load account into new object
-	If enough credit, deduct/add amount update and save. Display "AUTHORIZATION GRANTED"
-	If not enough credit, display "AUTHORIZATION DENIED"
-	If not, display "ACCOUNT NOT ON FILE"
-	*/
-	ifstream inFile;
-	inFile.open(fileName);
-	inFile >> *this;
-
-
+public: CreditCard(string accountNumber, double maxCredit, double curCredit) {
+	
+	this->accountNumber = accountNumber;
+	this->availableCredit = curCredit;
+	this->maxCredit = maxCredit;
+}
+public: void processTransaction(double amount) {
+	if (amount > this->availableCredit || this->availableCredit - amount > this->maxCredit) {
+		cout << "AUTHORIZATION DENIED";
+		return;
+	}
+	this->availableCredit -= amount;
 }
 
 private: string generateCardNumber(int firstDigit) {
@@ -97,38 +109,14 @@ private: string generateCardNumber(int firstDigit) {
 	return cardNumber;
 }
 
-public: string getAccountNumber() {
-	return accountNumber;
-}
-
 	friend ostream &operator << (ostream &out, const CreditCard& cc) {
 		out << cc.accountNumber << " " << cc.availableCredit << " " << cc.maxCredit << endl;
 		
 		return out;
 }
-	friend istream &operator >> (istream &in, CreditCard& cc) {
-		string line;
-		while (getline(in, line)) {
-			if (line.find(cc.accountNumber) != string::npos) {
-				stringstream linestream;
-
-				linestream << line;
-				string accountNumber;
-				string availableCredit;
-				string maxCredit;
-				linestream >> accountNumber >> availableCredit >> maxCredit;
-				cc.accountNumber = accountNumber;
-				cc.availableCredit = stof(availableCredit);
-				cc.maxCredit = stof(maxCredit);
-				return in;
-			}
-		}
-		cout << "ACCOUNT NOT FOUND";
-		return in;
-	}
-
-
 };
+
+string toUpper(string);
 
 int main(int argc, char** argv) {
 	const string FILE_NAME = "ccdb.txt";
@@ -152,8 +140,12 @@ int main(int argc, char** argv) {
 
 		string flag = (*(argv + 1));
 
+		flag = toUpper(flag);
+
 		if (flag.compare("CREATE") == 0) {
 			string symbol = (*(argv + 2));
+
+			symbol = toUpper(symbol);
 
 			CreditCard *cc;
 
@@ -175,7 +167,8 @@ int main(int argc, char** argv) {
 			else {
 				cout << "Invalid Symbol" << endl
 					<< "Use AE, V, MC, DIS, or DINE" << endl;
-				return;
+				_getch();
+				return 1;
 			}
 
 			ofstream outFile;
@@ -192,31 +185,77 @@ int main(int argc, char** argv) {
 		}
 		else {
 			cout << "Invalid argument: " << flag << endl;
+			return 1;
 		}
 	}
 
 	if (argc == 4) {
-		string accountNumber = (*(argv + 1));
-		if (accountNumber.compare("VERIFICATION") == 0) {
+		string flag = (*(argv + 1));
+		string accountNumber;
+		double amount;
 
+		flag = toUpper(flag);
 
-			double amount;
+		if (flag.compare("VERIFICATION") == 0) {
+
+			accountNumber = (*(argv + 2));
+
 
 			try {
-				amount = atof(*(argv + 2));
+				amount = atof(*(argv + 3));
 			}
 			catch(...){
 				cout << "AMOUNT must be only digits, decimals or a negative sign" << endl
 					<< "EX: \'-29.99\'" << endl;
 			}
-			CreditCard *cc = new CreditCard(accountNumber, amount, FILE_NAME);
 		}
+		
+		ifstream inFile(FILE_NAME);
+
+		if (!inFile) {
+			cout << "File not found. Please create an account first." << endl;
+			return 1;
+		}
+
+		string line;
+		while (getline(inFile, line)) {
+			if (line.find(accountNumber) != string::npos) {
+				istringstream iss(line);
+				double maxCredit;
+				double availCredit;
+				iss >> accountNumber >> availCredit >> maxCredit;
+				CreditCard cc(accountNumber, availCredit, maxCredit);
+				cc.processTransaction(amount);
+				return 0;
+			}
+		}
+
+		cout << "ACCOUNT NOT ON FILE";
+		return 1;
+
 	}
 
 	if (argc == 2 || argc > 4) {
-		// TODO - ERROR MESSAGE FOR INVALID ARGUMENT COUNT
+		cout << "Invalid number of arguments." << endl;
+		cout << "The following functions are available:" << endl;
+		cout << "CREATE [SYMBOL] - creates an account of the type specified by SYMBOL " << endl;
+		cout << "\tSymbols:" << endl
+			<< "\tAE - American Express" << endl
+			<< "\tV - Visa" << endl
+			<< "\tMC - MasterCard" << endl
+			<< "\tDIS - Discover" << endl
+			<< "\tDINE - Diner's Club" << endl;
+		cout << "VERIFICATION [ACCOUNT NUMBER] [AMOUNT] - attempts to charge or credit ACCOUNT NUMBER for AMOUNT" << endl
+			<< "Use negative numbers to credit to account (i.e. process payments or reunds)" << endl;
+		return 1;
 	}
 
 	_getch();
 	return 0;
+}
+
+string toUpper(string in) {
+	for (char a : in) {
+		a = toupper(a);
+	}
 }
